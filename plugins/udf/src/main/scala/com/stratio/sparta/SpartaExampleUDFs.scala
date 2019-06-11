684b5dab942d928a3e79138e099c6adf9244e67c
mtelloz <ma.tello.ortiz@gmail.com>
2019-06-11 13:12:31 +0200

    [SPARTA-NN] Fix in UDF module

--- plugins/udf/src/main/scala/com/stratio/sparta/SpartaExampleUDFs.scala
+++ plugins/udf/src/main/scala/com/stratio/sparta/SpartaExampleUDFs.scala
@@ -4,8 +4,9 @@
  * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
  */
 
-package com.stratio.sparta.sdk.lite.common
+package com.stratio.sparta
 
+import com.stratio.sparta.sdk.lite.common.SpartaUDF
 import org.apache.spark.sql.catalyst.ScalaReflection
 import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.types.StringType
