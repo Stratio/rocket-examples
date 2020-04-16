2413b551779afa1a70bb7c7f83b0f4d6a797faf1
darroyocazorla <darroyocazorla@gmail.com>
2020-04-16 11:29:32 +0200

    add rocket plugins

---
+++ rocket-plugins/transformation-lite-xd/src/main/scala/com/stratio/sparta/transformations/tokenizer/TokenizerTransformStepStreaming.scala
@@ -0,0 +1,74 @@
+package com.stratio.sparta.transformations.tokenizer
+
+import com.stratio.sparta.sdk.lite.streaming.LiteCustomStreamingTransform
+import com.stratio.sparta.sdk.lite.streaming.models.{OutputStreamingTransformData, ResultStreamingData}
+import com.stratio.sparta.sdk.lite.validation.ValidationResult
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.crossdata.XDSession
+import com.stratio.sparta.properties.ValidatingPropertyMap._
+import org.apache.spark.sql.types.{StringType, StructField, StructType}
+import org.apache.spark.streaming.StreamingContext
+
+
+import scala.util.Try
+
+class TokenizerTransformStepStreaming(
+                                       xdSession: XDSession,
+                                       streamingContext: StreamingContext,
+                                       properties: Map[String, String]
+                                     ) extends LiteCustomStreamingTransform(xdSession, streamingContext, properties) {
+
+  lazy val splitByChar: Option[String] = Try(Option(properties.getString("charPattern"))).getOrElse(None).notBlank
+  lazy val inputField : Option[String] = Try(Option(properties.getString("inputField"))).getOrElse(None).notBlank
+  lazy val outputField1: Option[String] = Try(Option(properties.getString("outputField1"))).getOrElse(None).notBlank
+  lazy val outputField2: Option[String] = Try(Option(properties.getString("outputField2"))).getOrElse(None).notBlank
+
+  override def validate(): ValidationResult = {
+    var validation = ValidationResult(valid = true, messages = Seq.empty)
+
+    if (splitByChar.isEmpty) {
+      validation = ValidationResult(
+        valid = false,
+        messages = validation.messages :+ "The char used to split the sentence must be defined with the option key 'charPattern'")
+    }
+
+    if (inputField.isEmpty) {
+      validation = ValidationResult(
+        valid = false,
+        messages = validation.messages :+ "Indicate the input field name with the option key 'inputField'")
+    }
+
+    if (outputField1.isEmpty) {
+      validation = ValidationResult(
+        valid = false,
+        messages = validation.messages :+ "Specify an option key 'outputField1' with the name of the first column")
+    }
+
+    if (outputField2.isEmpty) {
+      validation = ValidationResult(
+        valid = false,
+        messages = validation.messages :+ "Specify an option key 'outputField2' with the name of the second column")
+    }
+
+    validation
+  }
+
+  override def transform(inputData: Map[String, ResultStreamingData]): OutputStreamingTransformData = {
+    val inputStream = inputData.head._2.data
+    val newFields = Seq(
+      StructField(outputField1.get, StringType),
+      StructField(outputField2.get, StringType)
+    )
+    val fieldToOperate = inputData.head._2.schema.get.fieldIndex(inputField.get)
+    val splitByCharGet = splitByChar.get.charAt(0)
+    val newSchema = Option(StructType(newFields))
+
+    val result = inputStream.map{ row =>
+      val splitValues = row.get(fieldToOperate).toString.split(splitByCharGet)
+
+      Row.fromSeq(splitValues)
+    }
+
+    OutputStreamingTransformData(result, newSchema)
+  }
+}
---
+++ rocket-plugins/udf/src/main/scala/com/stratio/sparta/SpartaExampleUDFs.scala
@@ -0,0 +1,48 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
+ *
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
+ */
+
+package com.stratio.sparta
+
+import com.stratio.sparta.sdk.lite.common.SpartaUDF
+import org.apache.spark.sql.catalyst.ScalaReflection
+import org.apache.spark.sql.expressions.UserDefinedFunction
+import org.apache.spark.sql.types.StringType
+
+import scala.util.Try
+
+case class ToUpperCaseUDF() extends SpartaUDF {
+
+  val name = "uppercaseSparta"
+
+  val upper: String => String = _.toUpperCase
+
+  val userDefinedFunction: UserDefinedFunction =
+    UserDefinedFunction(upper , StringType, Option(Seq(StringType)))
+}
+
+case class ConcatUDF() extends SpartaUDF {
+
+  val name = "concatSparta"
+
+  val upper: (String, String) => String =  { case (str1, str2) =>
+    s"$str1/$str2"
+  }
+
+  val userDefinedFunction: UserDefinedFunction =
+    UserDefinedFunction(upper , StringType, Option(Seq(StringType, StringType)))
+}
+
+case class ToUpperCaseWithReflectionUDF() extends SpartaUDF {
+
+  val name = "upperCaseReflect"
+
+  val upper: String => String = _.toUpperCase
+
+  val userDefinedFunction: UserDefinedFunction = {
+    val inputTypes = Try(ScalaReflection.schemaFor(ScalaReflection.localTypeOf[String]).dataType :: Nil).toOption
+    UserDefinedFunction(upper , ScalaReflection.schemaFor(ScalaReflection.localTypeOf[String]).dataType, inputTypes)
+  }
+}
