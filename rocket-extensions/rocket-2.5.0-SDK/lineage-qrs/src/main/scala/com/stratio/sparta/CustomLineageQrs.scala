df52da7323ddcf413658ea778808124bb1816646
Jose Carlos Garcia Serrano <gserranojc@gmail.com>
2022-05-20 07:21:34 +0200

    more examples

--- rocket-extensions/rocket-2.5.0-SDK/lineage-qrs/src/main/scala/com/stratio/sparta/CustomLineageQrs.scala
+++ rocket-extensions/rocket-2.5.0-SDK/lineage-qrs/src/main/scala/com/stratio/sparta/CustomLineageQrs.scala
@@ -8,9 +8,51 @@ package com.stratio.sparta
 
 object CustomLineageQrs {
 
-  def getGovernanceMetadata(options: Map[String, String]): Map[String, String] =
+  def getHDFSMetadata(options: Map[String, String]): Map[String, String] =
     Map(
       "metadataPath" -> "s000001-hdfs1.s000001://tmp/tablaVolcado>/:tablaVolcado:",
       "dataStoreType" -> "HDFS"
     )
+
+  def getHDFSMetadataFromOptions(options: Map[String, String]): Map[String, String] = {
+    val path = options.getOrElse("path", "/tmp")
+    val metadataPath = options.get("tableName") match {
+      case Some(tableName) =>
+        s"s000001-hdfs1.s000001:/$path/$tableName>/:$tableName:"
+      case None =>
+        val file = path.split("/").last
+        s"s000001-hdfs1.s000001:/$path>/:$file:"
+    }
+
+    Map(
+      "metadataPath" -> metadataPath,
+      "dataStoreType" -> "HDFS"
+    )
+  }
+
+  def getJDBCMetadata(options: Map[String, String]): Map[String, String] =
+    Map(
+      "metadataPath" -> "s000001-postgresqa.s000001://rocket-nightly>/:enriched.users_with_diagnosis:",
+      "dataStoreType" -> "SQL"
+    )
+
+  def getJDBCMetadataFromOptions(options: Map[String, String]): Map[String, String] = {
+    (options.get("tableName"), options.get("dbtable")) match {
+      case (Some(tableName), _) =>
+        Map(
+          "metadataPath" -> s"s000001-postgresqa.s000001://rocket-nightly>/:$tableName:",
+          "dataStoreType" -> "SQL"
+        )
+
+      case (_, Some(sparkDbTable)) =>
+        Map(
+          "metadataPath" -> s"s000001-postgresqa.s000001://rocket-nightly>/:$sparkDbTable:",
+          "dataStoreType" -> "SQL"
+        )
+
+      case _ =>
+        Map.empty
+    }
+
+  }
 }
